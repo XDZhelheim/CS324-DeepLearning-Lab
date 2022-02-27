5f56247a222d987ed3ce62308851c65f36df78f5
XDZhelheim <601251915@qq.com>
2022-02-27 22:13:52 +0800

    task2 forward

--- Assignment1/Part2/mlp_numpy.py
+++ Assignment1/Part2/mlp_numpy.py
@@ -2,10 +2,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from modules import * 
+from modules import *
+
 
 class MLP(object):
-
     def __init__(self, n_inputs, n_hidden, n_classes):
         """
         Initializes multi-layer perceptron object.    
@@ -14,6 +14,17 @@ class MLP(object):
             n_hidden: list of integers, where each integer is the number of units in each linear layer
             n_classes: number of classes of the classification problem (i.e., output dimension of the network)
         """
+        self.n_inputs = n_inputs
+        self.n_hidden = n_hidden
+        self.n_classes = n_classes
+
+        self.layers = []
+        for num_units in n_hidden:
+            self.layers.append(Linear(n_inputs, num_units))
+            self.layers.append(ReLU())
+        self.layers.append(Linear(n_hidden[-1], n_classes))
+        self.layers.append(SoftMax())
+
     def forward(self, x):
         """
         Predict network output from input by passing it through several layers.
@@ -22,6 +33,9 @@ class MLP(object):
         Returns:
             out: output of the network
         """
+        out = x
+        for layer in self.layers:
+            out = layer.forward(out)
         return out
 
     def backward(self, dout):
@@ -30,4 +44,12 @@ class MLP(object):
         Args:
             dout: gradients of the loss
         """
-        return
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+        return dout
+
+if __name__ == "__main__":
+    mlp = MLP(2, [5], 2)
+    print(mlp.forward(np.array([[1, 2], [1, 2]])))
+    print(mlp.backward(np.array([[1.2, 0], [1.2, 0]])))
+    
--- Assignment1/Part2/modules.py
+++ Assignment1/Part2/modules.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Linear(object):
     def __init__(self, in_features, out_features):
         """
@@ -13,7 +14,15 @@ class Linear(object):
         2) Initialize biases self.params['bias'] with 0. 
         3) Initialize gradients with zeros.
         """
-        
+        self.params = {}
+        self.params["weight"] = np.random.normal(loc=0,
+                                                 scale=0.0001,
+                                                 size=(in_features,
+                                                       out_features))
+        self.params["bias"] = np.zeros(out_features)
+
+        self.grads = {}
+
     def forward(self, x):
         """
         Forward pass (i.e., compute output from input).
@@ -24,7 +33,11 @@ class Linear(object):
         Hint: Similarly to pytorch, you can store the computed values inside the object
         and use them in the backward pass computation. This is true for *all* forward methods of *all* modules in this class
         """
-        return out
+        self.x = x
+        self.out = x @ self.params["weight"] + self.params["bias"]
+        
+        print("out shape", self.out.shape)
+        return self.out
 
     def backward(self, dout):
         """
@@ -37,7 +50,13 @@ class Linear(object):
         Implement backward pass of the module. Store gradient of the loss with respect to 
         layer parameters in self.grads['weight'] and self.grads['bias']. 
         """
+        self.grads["weight"] = self.x.T @ dout
+        self.grads["bias"] = dout
+        dx = dout @ self.params["weight"].T
+        
+        print("dx shape", dx.shape)
         return dx
+
 
 class ReLU(object):
     def forward(self, x):
@@ -48,7 +67,10 @@ class ReLU(object):
         Returns:
             out: output of the module
         """
-        return out
+        self.out = np.maximum(0, x)
+
+        print("out shape", self.out.shape)
+        return self.out
 
     def backward(self, dout):
         """
@@ -58,7 +80,11 @@ class ReLU(object):
         Returns:
             dx: gradients with respect to the input of the module
         """
+        dx = np.where(self.out > 0, dout, 0)
+        
+        print("dx shape", dx.shape)
         return dx
+
 
 class SoftMax(object):
     def forward(self, x):
@@ -75,7 +101,11 @@ class SoftMax(object):
         https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
         
         """
-        return out
+        y = np.exp(x - np.max(x))
+        self.out = y / np.sum(y, axis=1).reshape(-1, 1)
+        
+        print("out shape", self.out.shape)
+        return self.out
 
     def backward(self, dout):
         """
@@ -85,7 +115,13 @@ class SoftMax(object):
         Returns:
             dx: gradients with respect to the input of the module
         """
+        # dx = np.diagflat(self.out) - np.outer(self.out, self.out)
+        # dx = dout @ dx  #!
+        dx = (dout - np.reshape(np.sum(dout * self.out, 1), [-1, 1])) * self.out
+        
+        print("dx shape", dx.shape)
         return dx
+
 
 class CrossEntropy(object):
     def forward(self, x, y):
@@ -97,6 +133,9 @@ class CrossEntropy(object):
         Returns:
             out: cross entropy loss
         """
+        out = -np.sum(y * np.log(x + 1e-15))  # avoid zero
+        
+        print("out shape", out.shape)
         return out
 
     def backward(self, x, y):
@@ -108,4 +147,7 @@ class CrossEntropy(object):
         Returns:
             dx: gradient of the loss with respect to the input x.
         """
+        dx = -y / (x + 1e-15)  # avoid zero
+        
+        print("dx shape", dx.shape)
         return dx
