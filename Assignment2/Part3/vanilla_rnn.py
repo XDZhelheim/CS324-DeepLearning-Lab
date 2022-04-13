9d1a82c13e8bfe847e1d77955f14c3ac20f22793
XDZhelheim <601251915@qq.com>
2022-04-13 23:43:44 +0800

    finish part3 code

--- Assignment2/Part3/vanilla_rnn.py
+++ Assignment2/Part3/vanilla_rnn.py
@@ -4,15 +4,46 @@ from __future__ import print_function
 
 import torch
 import torch.nn as nn
+import math
+
 
 class VanillaRNN(nn.Module):
-
-    def __init__(self, seq_length, input_dim, hidden_dim, output_dim, batch_size):
+    def __init__(self, input_dim, hidden_dim, output_dim):
         super(VanillaRNN, self).__init__()
-        # Initialization here ...
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+
+        self.Whx = nn.Parameter(torch.empty(input_dim, hidden_dim))
+        self.Whh = nn.Parameter(torch.empty(hidden_dim, hidden_dim))
+        self.bh = nn.Parameter(torch.empty(hidden_dim))
+
+        self.Wph = nn.Parameter(torch.empty(hidden_dim, output_dim))
+        self.bo = nn.Parameter(torch.empty(output_dim))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        # https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/rnn.py
+        stdv = 1.0 / math.sqrt(self.hidden_dim) if self.hidden_dim > 0 else 0
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
 
     def forward(self, x):
-        # Implementation here ...
-        pass
-        
-    # add more methods here if needed
+        """
+        x: (batch_size, seq_len, input_dim)
+        input_dim = 1, no word embedding
+        """
+        batch_size = x.shape[0]
+        seq_len = x.shape[1]
+        x = x.view(batch_size, seq_len, self.input_dim)
+        h = torch.zeros(batch_size, self.hidden_dim)
+
+        for timestep in range(seq_len):
+            xt = x[:, timestep, :]
+            h = torch.tanh(xt @ self.Whx + h @ self.Whh + self.bh)
+
+        out = h @ self.Wph + self.bo
+        out = nn.functional.softmax(out, dim=1)
+        return out
