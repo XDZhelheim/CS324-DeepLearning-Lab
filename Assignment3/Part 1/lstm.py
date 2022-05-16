9188998573d54c17c7bff11a6fae4b0644803b3c
XDZhelheim <601251915@qq.com>
2022-05-16 12:15:17 +0800

    finish p1 code

--- Assignment3/Part 1/lstm.py
+++ Assignment3/Part 1/lstm.py
@@ -4,16 +4,79 @@ from __future__ import print_function
 
 import torch
 import torch.nn as nn
+from torch.nn.parameter import Parameter
+import math
 
 ################################################################################
 
-class LSTM(nn.Module):
 
-    def __init__(self, seq_length, input_dim, hidden_dim, output_dim, batch_size):
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
         super(LSTM, self).__init__()
-        # Initialization here ...
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+
+        self.Wgx = Parameter(torch.empty(input_dim, hidden_dim))
+        self.Wgh = Parameter(torch.empty(hidden_dim, hidden_dim))
+        self.bg = Parameter(torch.empty(hidden_dim))
+
+        self.Wix = Parameter(torch.empty(input_dim, hidden_dim))
+        self.Wih = Parameter(torch.empty(hidden_dim, hidden_dim))
+        self.bi = Parameter(torch.empty(hidden_dim))
+
+        self.Wfx = Parameter(torch.empty(input_dim, hidden_dim))
+        self.Wfh = Parameter(torch.empty(hidden_dim, hidden_dim))
+        self.bf = Parameter(torch.empty(hidden_dim))
+
+        self.Wox = Parameter(torch.empty(input_dim, hidden_dim))
+        self.Woh = Parameter(torch.empty(hidden_dim, hidden_dim))
+        self.bo = Parameter(torch.empty(hidden_dim))
+
+        self.Wph = Parameter(torch.empty(hidden_dim, output_dim))
+        self.bp = Parameter(torch.empty(output_dim))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        # https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/rnn.py
+        stdv = 1.0 / math.sqrt(self.hidden_dim) if self.hidden_dim > 0 else 0
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
 
     def forward(self, x):
-        # Implementation here ...
-        
-    # add more methods here if needed
+        """
+        x: (batch_size, seq_len, input_dim)
+        input_dim = 1, no word embedding
+        """
+        batch_size = x.shape[0]
+        seq_len = x.shape[1]
+        x = x.view(batch_size, seq_len, self.input_dim)
+
+        for timestep in range(seq_len):
+            xt = x[:, timestep, :]
+            if timestep == 0:
+                g = torch.tanh(xt @ self.Wgx + self.bg)
+
+                i = torch.sigmoid(xt @ self.Wix + self.bi)
+                f = torch.sigmoid(xt @ self.Wfx + self.bf)
+                o = torch.sigmoid(xt @ self.Wox + self.bo)
+
+                c = g * i
+                h = torch.tanh(c) * o
+            else:
+                g = torch.tanh(xt @ self.Wgx + h @ self.Wgh + self.bg)
+
+                i = torch.sigmoid(xt @ self.Wix + h @ self.Wih + self.bi)
+                f = torch.sigmoid(xt @ self.Wfx + h @ self.Wfh + self.bf)
+                o = torch.sigmoid(xt @ self.Wox + h @ self.Woh + self.bo)
+
+                c = g * i + c * f
+                h = torch.tanh(c) * o
+
+        out = h @ self.Wph + self.bp
+        out = torch.softmax(out, dim=1)
+
+        return out
+
--- Assignment3/Part 1/train.py
+++ Assignment3/Part 1/train.py
@@ -3,65 +3,270 @@ from __future__ import division
 from __future__ import print_function
 
 import argparse
-import time
 import numpy as np
 
 import torch
-from torch.utils.data import DataLoader
+import datetime
+import matplotlib.pyplot as plt
 
 from dataset import PalindromeDataset
 from lstm import LSTM
 
-def train(config):
+SEED = 220516
 
-    # Initialize the model that we are going to use
-    model = None  # fixme
 
-    # Initialize the dataset and data loader (leave the +1)
-    dataset = PalindromeDataset(config.input_length+1)
-    data_loader = DataLoader(dataset, config.batch_size, num_workers=1)
+def onehot_decode(label):
+    return torch.argmax(label, dim=1)
 
-    # Setup the loss and optimizer
-    criterion = None  # fixme
-    optimizer = None  # fixme
 
-    for step, (batch_inputs, batch_targets) in enumerate(data_loader):
+def accuracy(predictions, targets):
+    """
+    Computes the prediction accuracy, i.e., the average of correct predictions
+    of the network.
+    Args:
+        predictions: 2D float array of size [number_of_data_samples, n_classes]
+        labels: 2D int array of size [number_of_data_samples, n_classes] with one-hot encoding of ground-truth labels
+    Returns:
+        accuracy: scalar float, the accuracy of predictions.
+    """
 
-        # Add more code here ...
+    pred_decode = onehot_decode(predictions)
+    true_decode = targets
 
-        # the following line is to deal with exploding gradients
-        torch.nn.utils.clip_grad_norm(model.parameters(), max_norm=config.max_norm)
+    assert (len(pred_decode) == len(true_decode))
 
-        # Add more code here ...
+    acc = torch.mean((pred_decode == true_decode).float())
 
-        loss = np.inf   # fixme
-        accuracy = 0.0  # fixme
+    return float(acc)
 
-        if step % 10 == 0:
-            # print acuracy/loss here
 
-        if step == config.train_steps:
-            # If you receive a PyTorch data-loader error, check this bug report:
-            # https://github.com/pytorch/pytorch/pull/9655
+@torch.no_grad()
+def eval_model(model, criterion, x, y):
+    out = model.forward(x)
+    loss = criterion.forward(out, y)
+    acc = accuracy(out, y)
+
+    return float(loss), float(acc)
+
+
+def train(model,
+          trainset,
+          evalset,
+          criterion,
+          optimizer,
+          batch_size,
+          train_steps,
+          eval_steps,
+          max_norm,
+          verbose,
+          num_workers=0,
+          visual_model=False,
+          quiet=False,
+          gpu=False):
+
+    if visual_model:
+        train_loss_list = []
+        train_acc_list = []
+        eval_loss_list = []
+        eval_acc_list = []
+
+    if gpu and torch.cuda.is_available():
+        print("---Using CUDA---")
+        model.cuda()
+    else:
+        if gpu and not torch.cuda.is_available():
+            print("Warning: CUDA not available.")
+        print("---Using CPU---")
+    # print(model)
+
+    trainset_loader = torch.utils.data.DataLoader(trainset,
+                                                  batch_size=batch_size,
+                                                  num_workers=num_workers)
+    evalset_loader = torch.utils.data.DataLoader(evalset,
+                                                 batch_size=batch_size,
+                                                 num_workers=num_workers)
+
+    # print("Train set shape", trainset.data.shape)
+    # num_batches = len(trainset) // batch_size
+    # print("Num of batches =", num_batches)
+
+    np.random.seed(SEED)
+    loss_sum = 0
+    acc_sum = 0
+    for step, (x_batch, y_batch) in enumerate(trainset_loader):
+        if gpu and torch.cuda.is_available():
+            x_batch = x_batch.cuda()
+            y_batch = y_batch.cuda()
+            
+        out_batch = model.forward(x_batch)
+        acc_sum += accuracy(out_batch, y_batch)
+
+        loss = criterion.forward(out_batch, y_batch)
+        loss_sum += loss.item()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=max_norm)
+
+        if (step + 1) % verbose == 0:
+            # train loss and acc
+            train_loss = loss_sum / verbose
+            train_acc = acc_sum / verbose
+            
+            if not quiet or visual_model:
+                # eval loss and acc
+                eval_loss, eval_acc = 0, 0
+                for eval_step, (eval_x_batch,
+                                eval_y_batch) in enumerate(evalset_loader):
+                    if gpu and torch.cuda.is_available():
+                        eval_x_batch = eval_x_batch.cuda()
+                        eval_y_batch = eval_y_batch.cuda()
+                    eval_loss_batch, eval_acc_batch = eval_model(
+                        model, criterion, eval_x_batch, eval_y_batch)
+
+                    eval_loss += eval_loss_batch
+                    eval_acc += eval_acc_batch
+
+                    if eval_step >= eval_steps - 1:
+                        break
+                eval_loss /= eval_steps
+                eval_acc /= eval_steps
+
+            if not quiet:
+                print(datetime.datetime.now(), "Step", step + 1,
+                      "\tTrain Loss = %.5f" % train_loss,
+                      "Train acc = %.3f " % train_acc,
+                      "Eval Loss = %.5f" % eval_loss,
+                      "Eval acc = %.3f " % eval_acc)
+
+            if visual_model:
+                train_loss_list.append(train_loss)
+                train_acc_list.append(train_acc)
+                eval_loss_list.append(eval_loss)
+                eval_acc_list.append(eval_acc)
+                
+            loss_sum = 0
+            acc_sum = 0
+
+        if step >= train_steps - 1:
             break
 
-    print('Done training.')
+    if visual_model:
+        plt.plot(np.arange(verbose, step + 2, step=verbose),
+                 train_loss_list,
+                 "-",
+                 label="train_loss")
+        plt.plot(np.arange(verbose, step + 2, step=verbose),
+                 eval_loss_list,
+                 "-",
+                 label="eval_loss")
+        plt.title("Step-Loss")
+        plt.xlabel("Step")
+        plt.ylabel("Loss")
+        plt.legend()
+        plt.show()
+
+        plt.plot(np.arange(verbose, step + 2, step=verbose),
+                 train_acc_list,
+                 "-",
+                 label="train_acc")
+        plt.plot(np.arange(verbose, step + 2, step=verbose),
+                 eval_acc_list,
+                 "-",
+                 label="eval_acc")
+        plt.title("Step-Accuracy")
+        plt.xlabel("Step")
+        plt.ylabel("Accuracy")
+        plt.legend()
+        plt.show()
+
+
+def main(args):
+    model = LSTM(args.input_dim, args.num_hidden, args.num_classes)
+
+    trainset = PalindromeDataset(args.input_length + 1)
+    testset = PalindromeDataset(args.input_length + 1)
+
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.RMSprop(model.parameters(), lr=args.learning_rate)
+
+    train(model,
+          trainset,
+          testset,
+          criterion=criterion,
+          optimizer=optimizer,
+          batch_size=args.batch_size,
+          train_steps=args.train_steps,
+          eval_steps=args.eval_steps,
+          max_norm=args.max_norm,
+          verbose=args.eval_freq,
+          num_workers=args.num_workers,
+          visual_model=args.visual_model,
+          quiet=args.quiet,
+          gpu=args.gpu)
+
 
 if __name__ == "__main__":
-
-    # Parse training configuration
     parser = argparse.ArgumentParser()
 
-    # Model params
-    parser.add_argument('--input_length', type=int, default=10, help='Length of an input sequence')
-    parser.add_argument('--input_dim', type=int, default=1, help='Dimensionality of input sequence')
-    parser.add_argument('--num_classes', type=int, default=10, help='Dimensionality of output sequence')
-    parser.add_argument('--num_hidden', type=int, default=128, help='Number of hidden units in the model')
-    parser.add_argument('--batch_size', type=int, default=128, help='Number of examples to process in a batch')
-    parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate')
-    parser.add_argument('--train_steps', type=int, default=10000, help='Number of training steps')
-    parser.add_argument('--max_norm', type=float, default=10.0)
+    parser.add_argument("--input_length",
+                        type=int,
+                        default=10,
+                        help="Length of an input sequence")
+    parser.add_argument("--input_dim",
+                        type=int,
+                        default=1,
+                        help="Dimensionality of input sequence")
+    parser.add_argument("--num_classes",
+                        type=int,
+                        default=10,
+                        help="Dimensionality of output sequence")
+    parser.add_argument("--num_hidden",
+                        type=int,
+                        default=128,
+                        help="Number of hidden units in the model")
+    parser.add_argument("--learning_rate",
+                        "-l",
+                        type=float,
+                        default=1e-3,
+                        help="Learning rate.")
+    parser.add_argument("--batch_size",
+                        "-b",
+                        type=int,
+                        default=128,
+                        help="Batch size")
+    parser.add_argument("--train_steps",
+                        "-s",
+                        type=int,
+                        default=10,
+                        help="Number of evaluation steps(use how many batches to evaluate).")
+    parser.add_argument("--eval_steps",
+                        "-e",
+                        type=int,
+                        default=1000,
+                        help="Number of training steps(not epoch).")
+    parser.add_argument("--max_norm", type=float, default=10.0)
+    parser.add_argument("--eval_freq",
+                        "-f",
+                        type=int,
+                        default=10,
+                        help="Frequency of evaluation on the test set.")
+    parser.add_argument(
+        "--num_workers",
+        "-w",
+        type=int,
+        default=0,
+        help="Param num_workers of pytorch. Set to 0 when using Jupyter!!!")
+    parser.add_argument("--visual_model",
+                        "-v",
+                        action="store_true",
+                        help="Visualize model after training.")
+    parser.add_argument("--quiet",
+                        "-q",
+                        action="store_true",
+                        help="No stdout when training.")
+    parser.add_argument("--gpu", "-g", action="store_true", help="Use GPU.")
 
-    config = parser.parse_args()
-    # Train the model
-    train(config)
+    args = parser.parse_args()
+    main(args)
